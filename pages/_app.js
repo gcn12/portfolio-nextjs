3f3a2322174d55622751a3a902d9a5cdd4f57800
Gareth Ng <garethcng@gmail.com>
2021-03-07 10:18:20 -0500

    added fonts

--- pages/_app.js
+++ pages/_app.js
@@ -22,10 +22,10 @@ const MyApp = ({ Component, pageProps }) => {
   }
   return (
     <div>
-      <Head>
+      {/* <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" /> 
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
-      </Head>
+      </Head> */}
       <Header 
         isModalOpen={isModalOpen}
         setIsModalOpen={setIsModalOpen} 
