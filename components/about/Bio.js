6d3c61905e4f4b977b562148dc5ed62bdf13bb80
Gareth Ng <garethcng@gmail.com>
2021-05-09 13:28:52 -0400

    updated resume

--- components/about/Bio.js
+++ components/about/Bio.js
@@ -12,7 +12,8 @@ import {
 const Bio = (props) => {
     return(
         <Container>
-            <Illustration onLoad={props.closeModal} src='https://firebasestorage.googleapis.com/v0/b/portfolio-assets.appspot.com/o/profile_small-2-2.jpg?alt=media&token=25e46a01-5444-437a-beca-f9b984852c73' />
+            <Illustration onLoad={props.closeModal} src='/profile.jpg' />
+            {/* <Illustration onLoad={props.closeModal} src='https://firebasestorage.googleapis.com/v0/b/portfolio-assets.appspot.com/o/profile_small-2-2.jpg?alt=media&token=25e46a01-5444-437a-beca-f9b984852c73' /> */}
             {/* <Illustration onLoad={props.closeModal} src='https://firebasestorage.googleapis.com/v0/b/portfolio-assets.appspot.com/o/about_image_small.png?alt=media&token=fb050f1b-2872-4767-9acd-6053172e5797' /> */}
             <div>
                 <LargeText>Thanks for stopping by!</LargeText>
