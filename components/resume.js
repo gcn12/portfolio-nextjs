aea5f8f6e59db4fc3d9cab863112540a14d63b86
Gareth Ng <63011139+gcn12@users.noreply.github.com>
2022-04-06 08:10:23 -0700

    Update resume.js

--- components/resume.js
+++ components/resume.js
@@ -1,2 +1,2 @@
 export const RESUME =
-  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets.appspot.com/o/Gareth%20Ng.pdf?alt=media&token=56f53f43-69fb-4fe6-a6b9-6c0d77c747eb";
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-assets.appspot.com/o/Gareth%20Ng.pdf?alt=media&token=a7f6b78c-771b-4452-97ad-aa390c71f323";
